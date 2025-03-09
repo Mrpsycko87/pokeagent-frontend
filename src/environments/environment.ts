a6080ed8a281e59a58e8bad1aacddd6e1147f667
Pablo Morales <pablomorgithub@gmail.com>
2025-03-08 20:01:54 -0500

    changes for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
-export const environment = {
+/* export const environment = {
     APIEndPoint: 'http://localhost:3000',
     production: false
+}; */
+export const environment = {
+    APIEndPoint: 'https://pokeagent-backend.onrender.com',
+    production: true
 };
+

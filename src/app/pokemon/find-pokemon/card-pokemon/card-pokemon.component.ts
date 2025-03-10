a35e06fdf030195b44e3fa7ee2689f979018a596
Pablo Morales <pablomorgithub@gmail.com>
2025-03-10 07:53:06 -0500

    targeta pokemon en mat-dialog

---
+++ src/app/pokemon/find-pokemon/card-pokemon/card-pokemon.component.ts
@@ -0,0 +1,80 @@
+import { Component, Inject } from '@angular/core';
+import { SharedModule } from '../../../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { PokemonService } from '../../../services/pokemon/pokemon.service';
+import Swal from 'sweetalert2'
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-card-pokemon',
+  imports: [SharedModule, ReactiveFormsModule, CommonModule],
+  templateUrl: './card-pokemon.component.html',
+  styleUrl: './card-pokemon.component.css'
+})
+export class CardPokemonComponent {
+  errorMessage: string = '';
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public dataJoin: any,
+    private pokeService: PokemonService,
+    private token: AuthService,
+    private router: Router
+  ) { }
+  
+
+  async capturePokemon(data: any) {
+    const token = this.token.getTokenstorage()
+    const { value: nickname } = await Swal.fire({
+      title: "¿Agregar Nickname al pokémon?",
+      input: "text",
+      inputLabel: "Si deja el espacio en blanco se guardará con el mismo nombre",
+      showCancelButton: false,
+      inputValidator: (value) => {
+        if (!value) {
+          data.nickname = data.name
+          data.user = token
+          this.pokeService.capturePokemon(data).subscribe({
+            next: (response) => {
+              Swal.fire({
+                imageUrl: "assets/img/pokeball-shimmer.gif",
+                imageWidth: 200,
+                imageHeight: 100,
+                title: `${response.name} ha sido capturado`,
+                icon: "success",
+                confirmButtonText: 'Entendido'
+              });
+              this.router.navigateByUrl('/pokemon/all')
+            },
+            error: (error) => {
+              this.errorMessage = error.error.message;
+            }
+          })
+        }
+      }
+    });
+    if (nickname) {
+      data.nickname = nickname
+      data.user = token
+      this.pokeService.capturePokemon(data).subscribe({
+        next: (response) => {
+          Swal.fire({
+            imageUrl: "assets/img/pokeball-shimmer.gif",
+            imageWidth: 200,
+            imageHeight: 100,
+            title: `${response.name} ha sido capturado`,
+            icon: "success",
+            confirmButtonText: 'Entendido'
+          });
+          this.router.navigateByUrl('/pokemon/all')
+        },
+        error: (error) => {
+          this.errorMessage = error.error.message;
+        }
+      })
+    }
+  }
+
+}
--- src/app/pokemon/find-pokemon/find-pokemon.component.ts
+++ src/app/pokemon/find-pokemon/find-pokemon.component.ts
@@ -3,9 +3,10 @@ import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { PokemonService } from '../../services/pokemon/pokemon.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CardPokemonComponent } from './card-pokemon/card-pokemon.component';
 import { SharedModule } from '../../material.module';
 import { CommonModule } from '@angular/common';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatDialog } from '@angular/material/dialog';
 import Swal from 'sweetalert2'
 
 @Component({
@@ -19,6 +20,7 @@ export class FindPokemonComponent implements OnInit {
   errorMessage: string = '';
   cardPokemon: boolean = false
   options_find = [{ option: 'N° Identificador', value: 'findById' }, { option: 'Nombre', value: 'findByName' }]
+
   public pokeForm = new FormGroup({
     id: new FormControl(),
     name: new FormControl(),
@@ -28,33 +30,11 @@ export class FindPokemonComponent implements OnInit {
   pokeFound: any
   constructor(
     private pokeService: PokemonService,
-    private token: AuthService,
-    private router: Router,
-    private breakpointObserver: BreakpointObserver
+    public dialog: MatDialog
   ) { }
-  ngOnInit() {
-    this.responsiveactive()
-  }
-  responsiveactive() {
-    this.breakpointObserver.observe([
-      Breakpoints.XSmall, // Teléfonos
-      Breakpoints.Small, // Tablets
-      Breakpoints.Medium, // Pantallas medianas
-      Breakpoints.Large // Pantallas grandes
-    ]).subscribe(result => {
-      if (result.matches) {
-        if (result.breakpoints[Breakpoints.XSmall]) {
-          this.cols = 1; // 1 columna en pantallas pequeñas
-        } else if (result.breakpoints[Breakpoints.Small]) {
-          this.cols = 1; // 1 columnas en tablets
-        } else if (result.breakpoints[Breakpoints.Medium]) {
-          this.cols = 2; // 2 columnas en pantallas medianas
-        } else {
-          this.cols = 2; // 2 columnas en pantallas grandes
-        }
-      }
-    })
-  }
+
+
+  ngOnInit() {}
 
   resetForm(ev: any) {
     ev.value == 'findByName' ? this.pokeForm.get('id')?.reset() : this.pokeForm.get('name')?.reset()
@@ -65,9 +45,8 @@ export class FindPokemonComponent implements OnInit {
     this.cardPokemon = false
     this.pokeService.findPokemon(this.pokeForm.getRawValue()).subscribe({
       next: (response) => {
-        this.cardPokemon = true
-        this.pokeFound = response
-        console.log('pokemon encontrado:', this.pokeFound);
+        console.log('pokemon encontrado:', response);
+        this.viewPokemonCard(response)
       },
       error: (error) => {
         this.errorMessage = error.error.message;
@@ -75,57 +54,12 @@ export class FindPokemonComponent implements OnInit {
     })
   }
 
-  async capturePokemon(data: any) {
-    const token = this.token.getTokenstorage()
-    const { value: nickname } = await Swal.fire({
-      title: "¿Agregar Nickname al pokémon?",
-      input: "text",
-      inputLabel: "Si deja el espacio en blanco se guardará con el mismo nombre",
-      showCancelButton: false,
-      inputValidator: (value) => {
-        if (!value) {
-          data.nickname = data.name
-          data.user = token
-          this.pokeService.capturePokemon(data).subscribe({
-            next: (response) => {
-              Swal.fire({
-                imageUrl: "assets/img/pokeball-shimmer.gif",
-                imageWidth: 200,
-                imageHeight: 100,
-                title: `${response.name} ha sido capturado`,
-                icon: "success",
-                confirmButtonText:'Entendido'
-              });
-              this.router.navigateByUrl('/pokemon/all')
-            },
-            error: (error) => {
-              this.errorMessage = error.error.message;
-            }
-          })
-        }
-      }
-    });
-    if (nickname) {
-      data.nickname = nickname
-      data.user = token
-      this.pokeService.capturePokemon(data).subscribe({
-        next: (response) => {
-          Swal.fire({
-            imageUrl: "assets/img/pokeball-shimmer.gif",
-            imageWidth: 200,
-            imageHeight: 100,
-            title: `${response.name} ha sido capturado`,
-            icon: "success",
-            confirmButtonText:'Entendido'
-          });
-          this.router.navigateByUrl('/pokemon/all')
-        },
-        error: (error) => {
-          this.errorMessage = error.error.message;
-        }
-      })
-    }
 
+  viewPokemonCard(data: any) {
+    const dialogRef = this.dialog.open(CardPokemonComponent, {
+      width: '500px',
+      data: data
+    })
+    dialogRef.afterClosed().subscribe(result => {})
   }
-
 }
